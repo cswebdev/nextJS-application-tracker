cc69b6a9932cc846e7bcb56d4412ff429fd11224
cswebdev <csnider91@live.com>
2024-11-15 13:22:40 -0500

    added svgr/webpack

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,38 @@ const nextConfig: NextConfig = {
    /* config options here */
    reactStrictMode: true,
 };
+module.exports = {
+   webpack(config: { module: { rules: any[]; }; }) {
+      // Grab the existing rule that handles SVG imports
+      const fileLoaderRule = config.module.rules.find((rule: { test: { test: (arg0: string) => any; }; }) =>
+         rule.test?.test?.(".svg")
+      );
+
+      config.module.rules.push(
+         // Reapply the existing rule, but only for svg imports ending in ?url
+         {
+            ...fileLoaderRule,
+            test: /\.svg$/i,
+            resourceQuery: /url/, // *.svg?url
+         },
+         // Convert all other *.svg imports to React components
+         {
+            test: /\.svg$/i,
+            issuer: fileLoaderRule.issuer,
+            resourceQuery: {
+               not: [...fileLoaderRule.resourceQuery.not, /url/],
+            }, // exclude if *.svg?url
+            use: ["@svgr/webpack"],
+         }
+      );
+
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+
+      return config;
+   },
+
+   // ...other config
+};
 
 export default nextConfig;
--- src/app/components/SideBar/Sidebar.tsx
+++ src/app/components/SideBar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import NavLinkMenu from "./SideBarNavMenu";
 import { Home, LogOutIcon, Settings, UserCircle } from "lucide-react";
-import logo from "../../../../public/pie-chart-svgrepo-com.svg";
+import WebLogo from "../../../../public/pie-chart-svgrepo-com.svg";
 
 interface SidebarProps {
    totalApplications?: number;
@@ -42,63 +42,72 @@ const Sidebar = ({ totalApplications }: SidebarProps) => {
    }, [totalApplications]);
 
    return (
-      <div className="h-full w-full bg-neutral-50 overflow-hidden ">
-         <section></section>
-         <div className="ml-8 flex h-full flex-col text-neutral-700 font-light ">
-            <div className="my-10 flex h-full flex-col  justify-center">
-               <Link href="/" className="my-2 pl-1 text-2xl">
-                  <div className="flex flex-row items-center">
-                     <span>
-                        <Home />
-                     </span>
-                     <h1 className="pl-2">Home</h1>
-                  </div>
-               </Link>
-               {/* how do I change this sections height depending on if NavLinkMenu is active and expanded?  */}
-               <section className="h-1/4">
-                  <NavLinkMenu />
-               </section>
-               <section className="">
-                  <div className="flex flex-row items-center mb-2">
-                     <span>
-                        <UserCircle />
-                     </span>
-                     <h2 className="text-2xl pl-2">Account</h2>
-                  </div>
-                  <div className="ml-9">
-                     <div className="flex flex-row items-center mb-2">
-                        <span>
-                           <Settings size={18} />
-                        </span>
-                        <Link href="settings" className="pl-2">
-                           Settings
-                        </Link>
-                     </div>
-                     <div className="flex flex-row items-center">
-                        <span>
-                           <LogOutIcon size={18} />
-                        </span>
-                        <div className="pl-2">Logout</div>
-                     </div>
-                  </div>
-               </section>
-
-               <div className="my-10">
-                  <div className="my-1 flex text-lg">
-                     Total applications: {totalApps}
-                  </div>
-                  <button className="mt-2">
-                     <Link
-                        href="/submit-application"
-                        className="mt-4 ring-2 ring-neutral-600 px-4 py-2 rounded-md"
-                     >
-                        Submit application
+      <>
+         <>
+            {/* <section className="w-full relative flex flex-col  p-0 size-20 bg-blue-200 object-contain">
+               <div className="absolute top-0 items-start object-scale-down">
+                  <WebLogo<any> />
+               </div>
+               <h1>Website Title</h1>
+            </section> */}
+            <div className="h-full w-full bg-red-50 overflow-hidden ">
+               <div className="ml-8 flex h-full flex-col text-neutral-700 font-light ">
+                  <div className="my-10 flex h-full flex-col  justify-center">
+                     <Link href="/" className="my-2 pl-1 text-2xl">
+                        <div className="flex flex-row items-center">
+                           <span>
+                              <Home />
+                           </span>
+                           <h1 className="pl-2">Home</h1>
+                        </div>
                      </Link>
-                  </button>
+                     {/* how do I change this sections height depending on if NavLinkMenu is active and expanded?  */}
+                     <section className="h-1/4">
+                        <NavLinkMenu />
+                     </section>
+                     <section className="">
+                        <div className="flex flex-row items-center mb-2">
+                           <span>
+                              <UserCircle />
+                           </span>
+                           <h2 className="text-2xl pl-2">Account</h2>
+                        </div>
+                        <div className="ml-9">
+                           <div className="flex flex-row items-center mb-2">
+                              <span>
+                                 <Settings size={18} />
+                              </span>
+                              <Link href="settings" className="pl-2">
+                                 Settings
+                              </Link>
+                           </div>
+                           <div className="flex flex-row items-center">
+                              <span>
+                                 <LogOutIcon size={18} />
+                              </span>
+                              <div className="pl-2">Logout</div>
+                           </div>
+                        </div>
+                     </section>
+
+                     <div className="my-10">
+                        <div className="my-1 flex text-lg">
+                           Total applications: {totalApps}
+                        </div>
+                        <button className="mt-2">
+                           <Link
+                              href="/submit-application"
+                              className="mt-4 ring-2 ring-neutral-600 px-4 py-2 rounded-md"
+                           >
+                              Submit application
+                           </Link>
+                        </button>
+                     </div>
+                  </div>
                </div>
             </div>
-         </div>
-      </div>
+         </>
+      </>
    );
 };
 
